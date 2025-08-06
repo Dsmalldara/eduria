7e35192fdbb383b5986be0af21d276656973238c
Darasimi <topedarasimi5@gmail.com>
2025-08-06 16:38:40 +0100

    deployment fix

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import {  createAuthClient } from "better-auth/client"
 
 export const {signIn, signUp, signOut, useSession, getSession } = createAuthClient({
-  baseURL: "https://eduria-385l.vercel.app/auth/login"
+  baseURL: "https://eduria-385l.vercel.app"
 })
 
 
